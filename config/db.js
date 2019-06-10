c24f8c083cf668ed431aedddd1083eac956df7e8
kshitiz20 <kshitizsharma62@gmail.com>
2019-06-10 12:26:44 +0000

    Changing the prod-> production

--- config/db.js
+++ config/db.js
@@ -1,4 +1,4 @@
-if(process.env.NODE_ENV=="prod"){
+if(process.env.NODE_ENV==="production"){
     module.exports={mongoURI:"mongodb://kshitiz:kshitiz0@ds227565.mlab.com:27565/youjot-prod"}
 }else{
     module.exports={mongoURI:"mongodb://localhost/youjot-dev"}
